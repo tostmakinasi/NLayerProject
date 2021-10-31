0cbfe8998c5e4c4240714132bf7d87e599b3b421
Kubilay Cem <kubilaycem@yandex.com>
2021-11-01 00:53:31 +0300

    IUnitOfWork interface tanımlanması ve Core katmanının genel anlamda tamamlanması

---
+++ NLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using NLayerProject.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.UnitOfWorks
+{
+    interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+
+        ICategoryRepository Categories { get; }
+
+        Task CommitAsync();
+
+        void Commit();
+
+
+    }
+}
